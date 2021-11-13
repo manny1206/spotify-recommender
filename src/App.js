e69ac72ec82660a8be2cf6bb711aba9d5dd3574b
Emmanuel <manny1206@hotmail.com>
2021-11-13 17:27:07 -0500

    changed App.js to functional style

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-class App extends Component {
+function App() {
   // Initialize state
-  state = { cities: [] }
+  const [cities, setCities] = useState([])
 
-  // Fetch passwords after first mount
-  componentDidMount() {
-    this.setCities();
-  }
+  const citez = ["Fairfax", "Vienna", "Falls Church", "Arlington"]
+  useEffect(() => {
+    setCities(citez)
+  })
 
-  setCities() {
-    const cities = ["Fairfax", "Vienna", "Falls Church", "Arlington"];
-    this.setState({ cities });
-  }
-
-render() {
-const { cities } = this.state;
-
-    return (
-      <div className="App">
-        {/* Render the cities*/}
-          <div>
-            <h1>Cities</h1>
-            <ul className="cities">
-              {cities.map((city, index) =>
-                <li key={index}>
-                  {city}
-                </li>
-              )}
-            </ul>
-          </div>
-          </div>
-      );    
-  }
+  return (
+    <div className="App">
+      {/* Render the cities*/}
+      <div>
+        <h1>Cities</h1>
+        <ul className="cities">
+          {cities.map((city, index) =>
+            <li key={index}>
+              {city}
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
-
-export default App;
+export default App

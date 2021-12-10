4031456779bbbd5688da24cb4f947a36eb5292cb
RonPhung <61999418+RonPhung@users.noreply.github.com>
2021-12-10 00:44:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,14 +20,17 @@ spot = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(clien
 # auth_manager = SpotifyClientCredentials
 # spot = spotipy.Spotify(auth_manager=auth_manager)
 
+# takes the id of a playlist and returns a dictionary of track ids and track features
 def retrieve_track_ids(playlist_id):
     results = spot.playlist_tracks(playlist_id)
     tracks = {}
 
+    # loop through the first 50 songs
     for index in range(50):
         if(index >= len(results["items"])):
             break
         try:
+            # stores a single track
             temp_track = results["items"][index]["track"]
 
             if(temp_track is None):
@@ -42,7 +45,6 @@ def retrieve_track_ids(playlist_id):
                 if(song_features is not None):
                     tracks[song_id] = song_features
 
-
         except spotipy.exceptions.SpotifyException:
             print("Spotipy Exception")
 
@@ -51,22 +53,26 @@ def retrieve_track_ids(playlist_id):
 
     return tracks
 
-
+# takes a string of the file name, and returns a dictionary of playlist id with a list of song ids
 def parse_spreadsheet(file):
     print("parse_spreadsheet")
     spreadsheet = pd.read_csv(file)
+    # sorts the playlist by the number of followers
     spreadsheet.sort_values(by="Followers")
     dataset = {}
 
     for index in range(100):
+        # gets the uri/id of a playlist
         playlist_id = spreadsheet["URL"][index][-22:]
+        # gets song id in the playlist
         dataset[playlist_id] = retrieve_track_ids(playlist_id)
 
-        if((index + 1) % 10):
+        if((index + 1) % 10 == 0):
             print("{}% complete".format(index + 1))
 
     return dataset
 
+# takes in a dictionary tha represents track features, and stores the desired variables into a list
 def extract_track_features(features):
     my_features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
                    'instrumentalness', 'liveness', 'valence', 'tempo', 'duration_ms', 'time_signature']
@@ -81,34 +87,40 @@ def extract_track_features(features):
 
     return result_features
 
-
+# takes in the data set and a song id and determines the playlist most similar to the song
 def determine_similarity(dataset, user_song_id):
     print("determine_similarity")
+    # gets the features of the given song
     user_track_feature = extract_track_features(spot.audio_features(user_song_id)[0])
     similarity_score = {}
 
+    # loops though each playlist in the data set
     for playlist in dataset.keys():
         playlist_similarity = 0
 
+        #loop through each song in the playlist
         for track in dataset[playlist].values():
+            # gets the features of a song in the playlist
             track_feature = extract_track_features(track)
-
+            # calculates how the distance/similarity of two songs features
             track_similarity = distance.euclidean(user_track_feature, track_feature)
+            # stores the sum of the similarity of all songs in the playlist
             playlist_similarity += track_similarity
 
+        # calculates the average similarity and store it with a playlist id
         similarity_score[playlist] = playlist_similarity / len(dataset[playlist])
 
-
+    # sorts the dictionary by the similarity (smalles - largest)(small == most similar)
     temp = sorted(similarity_score.items(), key=lambda x: x[1])
     return temp
 
+# takes a json/dictionary of the user's top songs and puts the song ids in a list
 def parse_user_top_songs(user_song_json):
     song_list = user_song_json["items"]
     user_songs = []
 
     for song in song_list:
         user_songs.append(song["id"])
-        print(song["name"])
 
     return user_songs
 
@@ -116,17 +128,18 @@ def main():
     start = time.time()
     print("Start")
 
-    with open('my_top_songs.txt') as json_file:
-        my_top_songs = json.load(json_file)
+    # comment this portion out to run the code
+    # with open('my_top_songs.txt') as json_file:
+    #     my_top_songs = json.load(json_file)
+    #
+    # user_song_list = parse_user_top_songs(my_top_songs)
+    # print(user_song_list)
 
-    user_song_list = parse_user_top_songs(my_top_songs)
-    print(user_song_list)
-
-    parse = False
+    parse = True
 
     song_list = ["6H3TW6uEe3RxW8CcnXJfq2", "6UelLqGlWMcVH1E5c4H7lY", "6D6HVKe7Qu3imn4zzJD0W9", "7w87IxuO7BDcJ3YUqCyMTT",
                  "5qEn8c0MBzyRKgQq91Vevi", "0GO8y8jQk1PkHzS31d699N", "152lZdxL1OR0ZMW6KquMif", "22UDw8rSfLbUsaAGTXQ4Z8",
-                 "77Ft1RJngppZlq59B6uP0z"]
+                 "77Ft1RJngppZlq59B6uP0z", "5HCyWlXZPP0y6Gqq8TgA20", "27NovPIUIRrOZoCHxABJwK", "3USxtqRwSYz57Ewm6wWRMp"]
 
     if(parse):
         print("Parsing data")
@@ -150,69 +163,6 @@ def main():
         # print("My song is {} by {}. My recommended playlist is {} - {}".format(spot.track(my_song)["name"], spot.track(my_song)["artists"][0]["name"], my_playlist["name"],my_playlist["external_urls"]["spotify"]))
         # results_file.write("My song is {} by {}. My recommended playlist is {} - {}\n".format(spot.track(my_song)["name"], spot.track(my_song)["artists"][0]["name"], my_playlist["name"],my_playlist["external_urls"]["spotify"]))
         results_file.write("{} by {} ---> {} {}\n".format(spot.track(my_song)["name"], spot.track(my_song)["artists"][0]["name"], my_playlist["name"], my_playlist["external_urls"]["spotify"]))
-
-
-    ###############
-    # with open("spotify/spotify_details.yml", 'r') as stream:
-    #     spotify_details = yaml.safe_load(stream)
-
-    # https://developer.spotify.com/web-api/using-scopes/
-    # scope = "user-library-read user-follow-read user-top-read playlist-read-private"
-    # sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    #     client_id=spotify_details['075c7aa80d9342b5a0773bedf7700940'],
-    #     client_secret=spotify_details['803fce8441af4f1ebef12b40c03bd0aa'],
-    #     redirect_uri=spotify_details['http://127.0.0.1:8080/'],
-    #     scope=scope, )
-    # )
-    # temp = sp.albums()
-
-    # print(temp)
-    ########################
-    # import tekore as tk
-    #
-    # client_id = 'your_id_here'
-    # client_secret = 'your_secret_here'
-    #
-    # app_token = tk.request_client_token(id, secret)
-    #
-    # auth = SpotifyOAuth(client_id=id, client_secret=secret, redirect_uri=url)
-    # sp = spotipy.Spotify(auth_manager=auth)
-    # playlist = sp.current_user_playlists()
-    # # playlist_url = playlist["items"][0]["external_urls"]
-    # # print(playlist_url)
-    #
-    # spotify = tk.Spotify(app_token)
-    #
-    #
-    # album = spotify.album('37i9dQZF1DX0XUsuxWHRQd')
-    # for track in album.tracks.items:
-    #     print(track.track_number, track.name)
-
-    ###################
-    # import spotipy
-    #
-    # urn = '3jOstUTkEu2JkjvRdBA5Gu'
-    # sp = spotipy.Spotify()
-    #
-    # artist = sp.artist(urn)
-    # print(artist)
-    #
-    # user = sp.user('plamere')
-    # print(user)
-
-    ######
-
-    # birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-
-
-    # albums = results['items']
-    # while results['next']:
-    #     results = spotify.next(results)
-    #     albums.extend(results['items'])
-    #
-    # for album in albums:
-    #     print(album['name'])
-
 
     print("Done")
     print(time.time() - start)

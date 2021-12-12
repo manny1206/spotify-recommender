c2ca53a5ff05ed38cfb8a1d7ff7510d80cebddb2
Emmanuel Jones <manny1206@hotmail.com>
2021-12-12 13:51:50 -0500

    work on the proxy configuration

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function(app) {
-    app.use('/authServer', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true })),
-    app.use('/recommenderServer', createProxyMiddleware({ target: 'https://spotify-recommender-server.herokuapp.com/', changeOrigin: true }))
+    app.use('/authServer', createProxyMiddleware({ target: 'https://spotify-recommender-ejrp.herokuapp.com:3001', changeOrigin: true })),
+    app.use('/recommenderServer', createProxyMiddleware({ target: 'https://spotify-recommender-server.herokuapp.com', changeOrigin: true }))
 }

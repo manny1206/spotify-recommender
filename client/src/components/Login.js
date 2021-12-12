0b23d77af4e8cd06f42c6bdd7f0aea6479414c01
Emmanuel Jones <manny1206@hotmail.com>
2021-12-12 13:55:33 -0500

    work on the proxy configuration

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,7 @@ import Container from "@mui/material/Container"
 import { Typography } from "@mui/material"
 
 const SPOTIFY_CLIENT_ID = "fc40f86251ce4a378422d00d57473fa1" // change later
-const SPOTIFY_REDIRECT_URI = "http://localhost:3000"
+const SPOTIFY_REDIRECT_URI = "https://spotify-recommender-ejrp.herokuapp.com"
 const SPOTIFY_SCOPE = "streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state%20user-top-read%20user-read-recently-played"
 const SPOTIFY_AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${SPOTIFY_REDIRECT_URI}&scope=${SPOTIFY_SCOPE}`
 
